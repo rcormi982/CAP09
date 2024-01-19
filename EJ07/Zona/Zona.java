b25a8047346b973866e50375025729f845842fc0
Rocio <rociocor77@gmail.com>
2024-01-19 09:42:50 +0100

    EJ07

---
+++ EJ07/Zona/Zona.java
@@ -0,0 +1,24 @@
+package EJ07.Zona;
+
+public class Zona {
+    private int entradasPorVender;
+    //Constructor
+    public Zona(int n){
+        entradasPorVender = n;
+    }
+    public int getEntradasPorVender() {
+        return entradasPorVender;
+    }
+    public void vender(int n){
+        if(this.entradasPorVender == 0){
+            System.out.println("Lo siento, las entradas para esa zona están agotadas");
+        }else if(this.entradasPorVender < 0){
+            System.out.println("Solo me quedan " + this.entradasPorVender + " entradas para esa zona");
+        }
+
+        if(this.entradasPorVender >=n){
+            entradasPorVender -=n;
+            System.out.println("Aquí tiene sus entradas " + n + " entradas, gracias");
+        }
+    }
+}
