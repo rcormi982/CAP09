0b2c495795a93927d6987f43f74351e555e942f6
Rocio <rociocor77@gmail.com>
2024-01-21 11:55:22 +0100

    EJ15

---
+++ EJ15/Ejercicio15.java
@@ -0,0 +1,8 @@
+/*Utiliza la clase anterior para generar una secuencia de 8 fichas creadas de
+forma aleatoria, que encajen bien y que estén bien colocadas según el juego
+del dominó. No hay que controlar si se repiten o no las fichas. */
+package EJ15;
+
+public class Ejercicio15 {
+    
+}
